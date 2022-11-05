e4591943fa14b2f53b5de270b56df4a047bcf422
Roman Kalinin <115572068+Pancu43@users.noreply.github.com>
2022-11-05 09:57:47 +0300

    Add element: swiper and tabs

--- Cid (first site)/js/main.js
+++ Cid (first site)/js/main.js
@@ -3,3 +3,33 @@ document.addEventListener ('DOMContentLoaded', function() {
     document.querySelector('header').classList.toggle('open')
   })
 })
+
+
+const swiper = new Swiper('.swiper-container', {
+  // Цикличность
+  loop: true,
+  // Пагинация
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+  },
+  a11y: {
+      paginationBulletMessage: 'Тут название слайда {{index}}',
+  }
+});
+
+let tabsBtn = document.querySelectorAll('.tabs-nav__btn');
+let tabsItem = document.querySelectorAll('.tabs__item');
+
+tabsBtn.forEach(function(element){
+  element.addEventListener('click', function(e){
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function(btn){ btn.classList.remove('tabs-nav__btn--active')});
+    e.currentTarget.classList.add('tabs-nav__btn--active');
+
+    tabsItem.forEach(function(element){ element.classList.remove('tabs__item--active')});
+    document.querySelector(`[data-target="${path}"]`).classList.add('tabs__item--active');
+  });
+});
+
