eb3f70392e55aa952f143164bbb7c3b12ab23e56
Roman Kalinin <115572068+Pancu43@users.noreply.github.com>
2022-11-08 15:21:02 +0300

    Cid - adding a search button

--- Cid (first site)/js/main.js
+++ Cid (first site)/js/main.js
@@ -1,9 +1,12 @@
+// бургер
+
 document.addEventListener ('DOMContentLoaded', function() {
   document.getElementById('burger').addEventListener('click', function() {
     document.querySelector('header').classList.toggle('open')
   })
 })
 
+// Swiper
 
 const swiper = new Swiper('.swiper-container', {
   // Цикличность
@@ -17,6 +20,9 @@ const swiper = new Swiper('.swiper-container', {
       paginationBulletMessage: 'Тут название слайда {{index}}',
   }
 });
+
+
+// табы
 
 let tabsBtn = document.querySelectorAll('.tabs-nav__btn');
 let tabsItem = document.querySelectorAll('.tabs__item');
@@ -33,4 +39,24 @@ tabsBtn.forEach(function(element){
   });
 });
 
+// аккордион
+
 new Accordion('.accordion');
+
+// кнопка поиска
+
+const searchPressSvg = document.querySelector( '.search-3__press-svg' );
+const searchInput = document.querySelector( '.search-3__input' );
+const searchBtn = document.querySelector( '.search-3__btn' );
+const search = document.querySelector( '.search-3' );
+const searchDiv = document.querySelector( '.search-3-div' );
+
+searchPressSvg.addEventListener( 'click', function(el) {
+  el.stopPropagation();
+  this.classList.add( 'search-3__press-svg--active' );
+  searchInput.classList.add( 'search-2__input--active' );
+  searchBtn.classList.add( 'search-3__btn--active' );
+  search.classList.add( 'search-3--active' );
+  searchDiv.classList.add( 'search-3-div--active' );
+
+} );
