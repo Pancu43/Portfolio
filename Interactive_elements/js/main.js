79e27442df2111a4ed341bcb13f26cf39bea0b69
Roman Kalinin <115572068+Pancu43@users.noreply.github.com>
2022-12-06 22:39:56 +0300

    done

--- Interactive_elements/js/main.js
+++ Interactive_elements/js/main.js
@@ -8,27 +8,49 @@ const choices = new Choices(element, {
 
 // валидация формы
 
-// var selector = document.getElementById("input[type='tel']");
-// var im = new Inputmask("+7 (999) 999-99-99");
+let validation = new JustValidate('#form')
 
-// im.mask(selector);
+let selector = document.querySelector('#tel')
+let im = new Inputmask("+7(999)999-99-99")
+im.mask(selector)
 
-// new JustValidate('.form', {
-//   rules: {
-//     name: {
-//       required: true,
-//       minLenght: 2,
-//       maxLenght: 10,
-//     },
-//     tel: {
-//       required: true,
-//       function: (name, value) => {
-//         const phone = selector.inputmask.unmaskedavalue ()
-//         return Number(phone) && phone.length === 10
-//       }
-//     }
-//   }
-// })
+validation.addField('#name', [
+  {
+    rule: 'required',
+    errorMessage: 'Вы не ввели имя'
+  },{
+    rule: 'minLength',
+    value: 2,
+    errorMessage: 'Минимум 2 символа'
+  },
+])
+.addField('#email', [
+  {
+    rule: 'required',
+    errorMessage: 'Вы не ввели почту'
+  },
+  {
+    rule: 'email',
+    errorMessage: 'Введите корректный e-mail'
+  },
+])
+.addField('#tel', [
+  {
+    validator: (value) => {
+      const tel = selector.inputmask.unmaskedvalue();
+      return Boolean(Number(tel) && tel.length > 0)
+    },
+    errorMessage: 'Вы не ввели телефон'
+  },
+  {
+    validator: (value) => {
+      const tel = selector.inputmask.unmaskedvalue();
+      return Boolean(Number(tel) && tel.length === 10)
+    },
+    errorMessage: 'Введите телефон полностью'
+  }
+])
+
 
 
 // тултип
